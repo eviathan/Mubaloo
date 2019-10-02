66f0568d951be52120106028e0c9f5d5d590dd03
Brian Williams <emial.brianwilliams@gmail.com>
2019-10-02 09:26:08 +0100

    Remove unused imports from NameFormField.js

--- ClientApp/src/components/Form/Fields/NameFormField.js
+++ ClientApp/src/components/Form/Fields/NameFormField.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { FormGroup, Col, Row } from 'reactstrap';
+import { FormGroup } from 'reactstrap';
 
 export class NameFormField extends Component {
 
